1340ac76e3ce00444dd4ad31dc1f5e39fda4e1ed
Al Bridges <albridges@Als-MacBook-Pro.local>
2018-03-28 23:18:07 -0400

    hw submit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Cheeseburger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hot N Now", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Le' Royale With Cheese", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("McBlt", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Taco Burger", TRUE);
+
+
